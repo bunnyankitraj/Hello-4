6b15e50d516af835ec1fce4345646b6e8394847e
Natalie Adams <natalie._.adams@hotmail.com>
2019-04-21 14:33:50 +1000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return(a+b)
+
+num1 = int(input("Please enter the first number"))
+num2 = int(input("Please enter the second number"))
+res = solveMeFirst(num1,num2)
+print(res)
